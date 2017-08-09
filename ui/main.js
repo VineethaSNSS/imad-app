1a2e2c989438c5c6954e45ffbb4a2c39747d3fbe
VineethaSNSS <30746272+VineethaSNSS@users.noreply.github.com>
2017-08-09 18:38:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,9 @@
 console.log('Loaded!');
+//change value
 var element=document.getElementById('main-text');
 element.innerHTML="new value";
+//move img
+var img=document.getElementById('img');
+img.onclick=function(){
+    img.style.marginLeft='100px';
+}
