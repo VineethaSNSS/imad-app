3aa39cb5888ae7a4ce9c09d5d9f05dc9b33dbc50
VineethaSNSS <30746272+VineethaSNSS@users.noreply.github.com>
2017-08-09 18:45:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,12 @@ var element=document.getElementById('main-text');
 element.innerHTML="new value";
 //move img
 var img=document.getElementById('madi');
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+ 'px';
+}
 img.onclick=function(){
-    img.style.marginLeft='100px';
+    var interval=setInterval(moveRight,100);
+    //img.style.marginLeft='100px';
 }
