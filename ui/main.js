4f97015139bd88e0dc9ca3d72e185781a2d79bab
VineethaSNSS <30746272+VineethaSNSS@users.noreply.github.com>
2017-08-09 19:17:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,8 @@ console.log('Loaded!');
 var counter=0;
 button.onClick=function(){
     
+     counter=counter+1;
     var span=document.getElementById('count');
-    counter=counter+1;
+   
     span.innerHTML = counter.toString();
 }
